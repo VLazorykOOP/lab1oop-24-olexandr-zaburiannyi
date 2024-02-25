ade428fbe71028cf12c5023f73a40414e8b5dc3b
olexandr-zaburiannyi <159687136+olexandr-zaburiannyi@users.noreply.github.com>
2024-02-25 14:11:48 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int firstTask() {
+  int N;
+  int numberOfZeros = 0;
+  cout << "Enter N " << endl;
+  cin >> N;
+
+  int A[N];
+  cout << "Enter " << N << " integers:" << endl;
+
+  for (int i = 0; i < N; ++i) {
+    cout << "Enter element " << i + 1 << ": ";
+    cin >> A[i];
+  }
+
+  for (int i = 0; i < N; ++i) {
+    if(A[i] == 0){
+      numberOfZeros += 1;
+    }
+  }
+
+  return numberOfZeros;
+}
+
+void secondTask() {
+int T;
+int size;
+int arr[size];
+int indexOfTEqual;
+int largestNegative;
+
+cout << "Enter T " << endl;
+cin >> T;
+
+cout << "Enter size " << endl;
+cin >> size;
+
+
+
+for (int i = 0; i < size; ++i) {
+  cout << "Enter element: " << endl;
+  cin >> arr[i];
+}
+for (int i = 0; i < size; ++i) {
+  if(arr[i] == T){
+    indexOfTEqual = i;
+  }
+}
+for (int i = indexOfTEqual; i < size; ++i) {
+  if (arr[i] < 0 && arr[i] > largestNegative) {
+      largestNegative = arr[i];
+  }
+}
+cout << "Largest negative: " << largestNegative << endl;
+}
+
+int main() {
+//  firstTask();
+//  secondTask();
+}
